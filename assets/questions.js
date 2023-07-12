f1e314d3ad1276c7a21271c3e050ba7be127653a
danie <danielfama14@gmail.com>
2023-07-12 16:06:03 -0700

    add JavaScript

---
+++ assets/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types do not include:",
+    answer: "Alerts",
+    options: [
+      "Strings",
+      "Booleans",
+      "Alerts",
+      "Numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed with ______",
+    answer: "Parenthesis",
+    options: [
+      "Quotes",
+      "Curly Brackets",
+      "Parenthesis",
+      "Square Brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store ______",
+    answer: "All of the above",
+    options: [
+      "Numbers and strings",
+      "Other arrays",
+      "Booleans",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed within ______ when being assigned to variables",
+    answer: "Quotes",
+    options: [
+      "Commas",
+      "Curly brackets",
+      "Quotes",
+      "Parenthesis"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A very useful too used during development and debugging for printing content to the debugger is:",
+    answer: "Console log",
+    options: [
+      "JavaScript",
+      "Terminal/bash",
+      "For loops",
+      "Console log"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
